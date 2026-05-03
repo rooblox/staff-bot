0e864ee307ba70d597bef7f89adf1175e8987a81
rooblox <34076536+rooblox@users.noreply.github.com>
2026-05-03 16:00:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1311,9 +1311,6 @@ client.once('ready', async () => {
     console.log(`✅ Ready event fired!`);
     const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-    await rest.put(Routes.applicationCommands(client.user.id), { body: [] });
-    console.log('✅ Cleared global commands');
-
     console.log(`✅ Guild cache size: ${client.guilds.cache.size}`);
     console.log(`✅ Starting guild registration loop...`);
 
